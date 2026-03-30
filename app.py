d353e6dc0094b5477ff35d695b5fad75bb526dac
Anastasia Irizarry <airizarry0777@floridapoly.edu>
2026-03-30 18:56:02 +0000

    Done

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 import pandas as pd
 import streamlit as st
+import logic
 
+st.title("Faculty Grade Dashboard")
 col1, col2, col3 = st.columns(3)
+average, highest, lowest = logic.calculate_stats(dataframe)
 
-col1.metric("Average", "Placeholder")
-col2.metric("Highest", "Placeholder")
-col3.metric("Lowest", "Placeholder")
+col1.metric("Average", f"{average:.1f}")
+col2.metric("Highest", highest)
+col3.metric("Lowest", lowest)
 
-current_roster = pd.DataFrame(
-    {
-        "Student": ["Placeholder"],
-        "Grade": ["Placeholder"],
-    },
-    index =["Placeholder"],
-)
-st.table(current_roster)
+st.table(dataframe)
