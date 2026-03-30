9df71f1f22736162c52054cc66f2e8160c8af3e3
Juliana Ariza <jariza2458@floridapoly.edu>
2026-03-30 18:30:13 +0000

    Logic.py finished

--- logic.py
+++ logic.py
@@ -1,11 +1,26 @@
 import pandas as pd
 
 def calculate_stats(df):
-    print("hello")
+    mean = float(df["Grades"].mean())
+    max = int(df["Grades"].max())
+    min = int(df["Grades"].min())
+    return mean, max, min
     
 
 def get_grade_distribution(df):
-    print("hello world")
+    grade_distribution = {'A': 0, 'B': 0,'C': 0, 'D': 0, 'F': 0}
+    grades = df["Grades"].to_list()
 
+    for grade in grades:
+        if grade >= 90:
+            grade_distribution['A'] = grade_distribution['A'] + 1
+        elif grade >= 80:
+            grade_distribution['B'] = grade_distribution['B'] + 1
+        elif grade >= 70:
+            grade_distribution['C'] = grade_distribution['C'] + 1
+        elif grade >= 60:
+            grade_distribution['D'] = grade_distribution['D'] + 1
+        else:
+            grade_distribution['F'] = grade_distribution['F'] + 1
 
-print("hello world")
+    return grade_distribution
