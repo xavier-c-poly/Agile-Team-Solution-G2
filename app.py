1958d985c69113c6a4e159759eefd553d1c2e50c
Xavier Cosme <xcosme1849@floridapoly.edu>
2026-03-30 18:34:47 +0000

    unfinished visualization section

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-import streamlit
+import streamlit as st
+
+# Use st.bar_chart to display the distribution data
+# provided by the logic module
+
+st.markdown("# Faculty Grade Dashboard")
+
+average = 3
+lowest = 1
+highest = 5
+
+st.markdown(f"#### Average: {average}")
+st.markdown(f"#### Highest: {highest}")
+st.markdown(f"#### Lowest: {lowest}")
+
+st.markdown("## Grade Distribution")
+st.bar_chart({
+    'A': 2,
+    'B': 0,
+    'C': 1,
+    'D': 1,
+    'F': 1
+})
+
+st.markdown("## Current Roster")
+st.dataframe("Jordan's dataframe goes here")
